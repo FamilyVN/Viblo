f2fd04c3b243f9978c39393be1ef19603cd8134d
TuanAnh <vtabk2@gmail.com>
2017-11-03 10:36:29 +0700

    ref add listener bottom navigation

--- app/src/main/java/com/asia/viblo/view/activity/home/MainActivity.kt
+++ app/src/main/java/com/asia/viblo/view/activity/home/MainActivity.kt
@@ -1,95 +1,33 @@
 package com.asia.viblo.view.activity.home
 
-import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
-import android.text.TextUtils
-import android.view.View
 import com.asia.viblo.R
-import com.asia.viblo.model.extraUrl
-import com.asia.viblo.model.keyMaxPage
-import com.asia.viblo.model.keyPagePresent
-import com.asia.viblo.model.post.Post
-import com.asia.viblo.utils.SharedPrefs
-import com.asia.viblo.view.activity.detail.PostDetailActivity
-import com.asia.viblo.view.adapter.PostAdapter
-import com.asia.viblo.view.asyncTask.*
+import com.asia.viblo.view.fragment.discussions.DiscussionsFragment
+import com.asia.viblo.view.fragment.post.PostFragment
+import com.asia.viblo.view.fragment.questions.QuestionsFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), OnClickPostDetail {
-    private val mPostList: MutableList<Post> = arrayListOf()
-    private lateinit var mPostAdapter: PostAdapter
-    private var mPosition: Int = 0
 
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initRecyclerPost()
-        updateNavigationBottom()
-        initListener()
+        initBottomNavigationView()
     }
 
-    private fun updateNavigationBottom() {
-        val pagePresentStr = SharedPrefs.instance[keyPagePresent, String::class.java]
-        val pageMaxStr = SharedPrefs.instance[keyMaxPage, String::class.java]
-        textPageBack.visibility = if (TextUtils.equals("1", pagePresentStr)) View.GONE else View.VISIBLE
-        textPageNext.visibility = if (TextUtils.equals(pageMaxStr, pagePresentStr)) View.GONE else View.VISIBLE
-        textPagePresent.text = getPagePresent(pagePresentStr, pageMaxStr)
-    }
-
-    private fun getPagePresent(pagePresentStr: String, pageMaxStr: String): String {
-        return pagePresentStr + "/" + pageMaxStr
-    }
-
-    private fun initRecyclerPost() {
-        mPostAdapter = PostAdapter(this, mPostList, this)
-        recyclerAllPost.adapter = mPostAdapter
-        recyclerAllPost.layoutManager = LinearLayoutManager(this)
-        loadData(baseUrlNewest, "")
-    }
-
-    private fun loadData(url: String, page: String) {
-        mPostList.clear()
-        if (TextUtils.isEmpty(page)) {
-            mPostList.addAll(LoadPostAsyncTask().execute(url).get())
-        } else {
-            mPostList.addAll(LoadPostAsyncTask().execute(url, page).get())
+    private fun initBottomNavigationView() {
+        bottomNavigationHome.selectedItemId = 0
+        bottomNavigationHome.setOnNavigationItemSelectedListener { item ->
+            val fragment: Fragment = when (item.itemId) {
+                R.id.itemPost -> PostFragment()
+                R.id.itemQuestions -> QuestionsFragment()
+                R.id.itemDiscussion -> DiscussionsFragment()
+                else -> PostFragment()
+            }
+            supportFragmentManager.beginTransaction().replace(R.id.frameHome, fragment).commit()
+            true
         }
-        mPostAdapter.notifyDataSetChanged()
-    }
-
-    private fun getLink(type: Int): String {
-        return when (type) {
-            0 -> baseUrlNewest
-            1 -> baseUrlSeries
-            2 -> baseUrlEditorsChoice
-            3 -> baseUrlTrending
-            4 -> baseUrlVideos
-            else -> baseUrlNewest
-        }
-    }
-
-    private fun initListener() {
-        textPageNext.setOnClickListener {
-            val pageNext = SharedPrefs.instance[keyPagePresent, String::class.java].toInt() + 1
-            loadData(getLink(mPosition), pageNext.toString())
-            updateNavigationBottom()
-        }
-        textPageBack.setOnClickListener {
-            val pageBack = SharedPrefs.instance[keyPagePresent, String::class.java].toInt() - 1
-            loadData(getLink(mPosition), pageBack.toString())
-            updateNavigationBottom()
-        }
-        textPagePresent.setOnClickListener {
-            loadData(getLink(mPosition), SharedPrefs.instance[keyPagePresent, String::class.java])
-            updateNavigationBottom()
-        }
-    }
-
-    override fun onClickPostDetail(url: String) {
-        val intent = Intent(this, PostDetailActivity::class.java)
-        intent.putExtra(extraUrl, url)
-        startActivity(intent)
     }
 }
