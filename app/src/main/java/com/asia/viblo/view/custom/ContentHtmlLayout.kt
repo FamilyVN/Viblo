7a0aef08ea304e564e6dda8840bcd6fe2eeb987b
TuanAnh <vtabk2@gmail.com>
2017-11-15 09:59:32 +0700

    ref update case block quote

--- app/src/main/java/com/asia/viblo/view/custom/ContentHtmlLayout.kt
+++ app/src/main/java/com/asia/viblo/view/custom/ContentHtmlLayout.kt
@@ -1,6 +1,7 @@
 package com.asia.viblo.view.custom
 
 import android.content.Context
+import android.os.Build
 import android.text.Html
 import android.text.TextUtils
 import android.util.AttributeSet
@@ -11,9 +12,10 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import com.asia.viblo.R
 import com.asia.viblo.model.ContentChild
-import com.asia.viblo.model.TypeContent
+import com.asia.viblo.model.TypeContent.*
 import com.asia.viblo.utils.getUrlListFromString
 import com.asia.viblo.utils.loadAvatar
+import kotlinx.android.synthetic.main.item_layout_content_default.view.*
 
 /**
  * Created by anhtv on 14/11/2017.
@@ -36,13 +38,25 @@ class ContentHtmlLayout : LinearLayout {
                     contentChildList.add(ContentChild(data))
                 }
                 data.contains("<img class=\"block-image\" src=") -> {
-                    contentChildList.add(ContentChild(data, TypeContent.IMAGE))
+                    contentChildList.add(ContentChild(data, IMAGE))
                 }
                 data.contains("<table>") -> {
-                    contentChildList.add(ContentChild(data, TypeContent.TABLE))
+                    contentChildList.add(ContentChild(data, TABLE))
+                }
+                data.contains("<blockquote>") -> {
+                    val content = data
+                            .replace("<blockquote>", "")
+                            .replace("</blockquote>", "")
+                    contentChildList.add(ContentChild(content, BLOCK_QUOTE))
                 }
                 data.contains("<pre><code class=") -> {
-                    contentChildList.add(ContentChild(data, TypeContent.CODE))
+                    contentChildList.add(ContentChild(data, CODE_CLASS))
+                }
+                data.contains("<code>") -> {
+                    val content = data
+                            .replace("<code>", "<font color = '#bd4147'>")
+                            .replace("</code>", "</font>")
+                    contentChildList.add(ContentChild(content, CODE))
                 }
                 else -> {
                     if (!TextUtils.isEmpty(data) && data.isNotBlank()) {
@@ -55,13 +69,15 @@ class ContentHtmlLayout : LinearLayout {
         for (contentChild: ContentChild in contentChildList) {
             Log.d("TAG", "content = " + contentChild.content)
             when (contentChild.typeContent) {
-                TypeContent.TEXT -> {
+                TEXT, CODE -> {
                     val textView = layoutInflater.inflate(
                             R.layout.item_text_content_default, this, false) as TextView
-                    textView.text = Html.fromHtml(contentChild.content)
+                    textView.setText(if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+                        Html.fromHtml(contentChild.content, Html.FROM_HTML_MODE_COMPACT) else
+                        Html.fromHtml(contentChild.content), TextView.BufferType.SPANNABLE)
                     addView(textView)
                 }
-                TypeContent.IMAGE -> {
+                IMAGE -> {
                     val list = contentChild.content.split("\"")
                     for (sub: String in list) {
                         val urlList = getUrlListFromString(sub)
@@ -73,9 +89,18 @@ class ContentHtmlLayout : LinearLayout {
                         }
                     }
                 }
-                TypeContent.TABLE -> {
+                TABLE -> {
                 }
-                TypeContent.CODE -> {
+                CODE_CLASS -> {
+
+                }
+                BLOCK_QUOTE -> {
+                    val layout = layoutInflater.inflate(
+                            R.layout.item_layout_content_default, this, false) as LinearLayout
+                    layout.txtContent.setText(if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+                        Html.fromHtml(contentChild.content, Html.FROM_HTML_MODE_COMPACT) else
+                        Html.fromHtml(contentChild.content), TextView.BufferType.SPANNABLE)
+                    addView(layout)
                 }
             }
         }
